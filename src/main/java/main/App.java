20698806d8acf844e46648b368b17bc251a0d554
tenhuner <63654635+tenhuner@users.noreply.github.com>
2024-07-17 12:05:21 +0300

    d

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -12,9 +12,7 @@ public class App {
         String university = scanner.nextLine();
 
         while (true) {
-            System.out.println("1) Luo uusi kurssi, 2) Luo uusi opiskelija, 3) Listaa kurssit, 4) Listaa opiskelijat, "
-                    + "5) Lisää opiskelija kurssille, 6) Anna kurssiarvosanat, 7) Listaa kurssilla olevat opiskelijat, "
-                    + "8) Listaa opiskelijan arvosanat, 9) Listaa kaikkien kurssien kaikkien opiskelijoiden arvosanat, 0) Lopeta ohjelma");
+            System.out.println("1) Luo uusi kurssi, 2) Luo uusi opiskelija, 3) Listaa kurssit, 4) Listaa opiskelijat, 5) Lisää opiskelija kurssille, 6) Anna kurssiarvosanat, 7) Listaa kurssilla olevat opiskelijat, 8) Listaa opiskelijan arvosanat, 9) Listaa kaikkien kurssien kaikkien opiskelijoiden arvosanat, 0) Lopeta ohjelma");
             int command = Integer.parseInt(scanner.nextLine());
 
             switch (command) {
